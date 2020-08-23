00c789bf8b168b3b0afefb66b212ce028634e5d0
= <>
2020-08-23 18:35:37 -0500

    added notes

--- bolt/working_file.py
+++ bolt/working_file.py
@@ -14,40 +14,9 @@ print("Hello")
   https://www.cyberciti.biz/faq/how-to-add-to-bash-path-permanently-on-linux/
 """
 
-
-# from time import sleep
-# 
-# from pysphero.utils import toy_scanner
-# 
-# 
-# def main():
-#     with toy_scanner(timeout=50.0) as sphero:
-#         sphero.power.wake()
-#         sleep(2)
-#         sphero.power.enter_soft_sleep()
-# 
-# 
-# if __name__ == '__main__':
-#     main()
-# 
-# from time import sleep
-# 
-# from pysphero.constants import Toy
-# from pysphero.utils import toy_scanner
-# from pysphero.core import Sphero
-# # https://github.com/EnotYoyo/pysphero/blob/master/pysphero/core.py
-# # ble_adapter_cls
-# def main():
-#     with Sphero(mac_address="DA:F4:35:74:09:03", toy_type=Toy.Sphero) as sphero:
-#         sphero.power.wake()
-#         sleep(2)
-#         sphero.power.enter_soft_sleep()
-#     pass
-# 
-# if __name__ == "__main__":
-#     main()
-
- 
+"""
+Imports
+"""
 import random
 from time import sleep
 
@@ -55,32 +24,78 @@ from pysphero.core import Sphero
 from pysphero.driving import Direction
 from pysphero.bluetooth.bluepy_adapter import BluepyAdapter
 
+
+"""
+Connectivity
+"""
+
+mac_address = "DA:F4:35:74:09:03"
+adapter = BluepyAdapter
+connect_to_bolt = lambda: Sphero(mac_address=mac_address, ble_adapter_cls=adapter)
+
+
+"""
+Working code to connect to bolt
+"""
+
+
+# def main():
+# 
+#     with Sphero(mac_address=mac_address, ble_adapter_cls=adapter) as sphero:
+#         sphero.power.wake()
+# 
+#         for _ in range(5):
+#             sleep(2)
+#             speed = random.randint(50, 100)
+#             heading = random.randint(0, 360)
+#             print(f"Send drive with speed {speed} and heading {heading}")
+# 
+#             sphero.driving.drive_with_heading(speed, heading, Direction.forward)
+# 
+#         sphero.power.enter_soft_sleep()
+# """
+# THIS WORKED
+# 
+# Steps I took (not sure which impacted the final solution)
+# 1. Installed dependencies: including gatt
+# 2. Added contents described at top of page to the path
+# 3. Scanned using bluetoothctl on the pi
+# 4. Modified starter code and specified the mac address and BluepyAdapter
+# 
+# """
+# 
+# if __name__ == "__main__":
+#     main()
+    
+    
+from time import sleep
+from typing import Dict
+
+from pysphero.core import Sphero
+from pysphero.device_api.sensor import CoreTime, Quaternion
+
+
+def notify_callback(data: Dict):
+    info = ", ".join("{:1.2f}".format(data.get(param)) for param in Quaternion)
+    print(f"[{data.get(CoreTime.core_time):1.2f}] Quaternion (x, y, z, w): {info}")
+    print("=" * 60)
+
+
 def main():
-    mac_address = "DA:F4:35:74:09:03"
-    adapter = BluepyAdapter
-    with Sphero(mac_address=mac_address, ble_adapter_cls=adapter) as sphero:
+    with connect_to_bolt() as sphero:
         sphero.power.wake()
-
-        for _ in range(5):
-            sleep(2)
-            speed = random.randint(50, 100)
-            heading = random.randint(0, 360)
-            print(f"Send drive with speed {speed} and heading {heading}")
-
-            sphero.driving.drive_with_heading(speed, heading, Direction.forward)
-
+        sphero.sensor.set_notify(notify_callback, CoreTime, Quaternion)
+        sleep(2)
+        sphero.sensor.cancel_notify_sensors()
         sphero.power.enter_soft_sleep()
-"""
-THIS WORKED
+# TODO Start here: Just got connectivity and reviewed the movement and sensor functions.
+#  Next step is to continue building a flask API with the functions needed for localization.
 
-Steps I took (not sure which impacted the final solution)
-1. Installed dependencies: including gatt
-2. Added contents described at top of page to the path
-3. Scanned using bluetoothctl on the pi
-4. Modified starter code and specified the mac address and BluepyAdapter
+# Future plan ideas:
+#  * Bolt is the pup, the stationary picamera and time-of-flight sensor work together to map area
 
-"""
 
 if __name__ == "__main__":
     main()
+
 
